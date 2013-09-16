a99461eef6304e8ac1a22b585fb5644992a64cf0
Eric Frommelt <ericfrommelt@gmail.com>
2013-09-16 12:18:48 -0700

    Added pages 18-20

---
+++ js/last-word.js
@@ -0,0 +1,82 @@
+$(document).ready(function(){
+	$(".profile-1").on("mouseenter", function() {
+    	$(".profile-1-quote").css("display", "block");
+    	$(".profile-1-color").css("top", "0");
+	});
+	$(".profile-1").on("mouseleave", function() {
+    	$(".profile-1-quote").css("display", "none");
+        $(".profile-1-color").css("top", "240px");
+	});
+
+    $(".profile-2").on("mouseenter", function() {
+        $(".profile-2-quote").css("display", "block");
+        $(".profile-2-color").css("top", "0");
+    });
+    $(".profile-2").on("mouseleave", function() {
+        $(".profile-2-quote").css("display", "none");
+        $(".profile-2-color").css("top", "240px");
+    });
+
+    $(".profile-3").on("mouseenter", function() {
+        $(".profile-3-quote").css("display", "block");
+        $(".profile-3-color").css("top", "0");
+    });
+    $(".profile-3").on("mouseleave", function() {
+        $(".profile-3-quote").css("display", "none");
+        $(".profile-3-color").css("top", "272px");
+    });
+
+    $(".profile-4").on("mouseenter", function() {
+        $(".profile-4-quote").css("display", "block");
+        $(".profile-4-color").css("top", "0");
+    });
+    $(".profile-4").on("mouseleave", function() {
+        $(".profile-4-quote").css("display", "none");
+        $(".profile-4-color").css("top", "272px");
+    });
+
+    $(".profile-5").on("mouseenter", function() {
+        $(".profile-5-quote").css("display", "block");
+        $(".profile-5-color").css("top", "0");
+    });
+    $(".profile-5").on("mouseleave", function() {
+        $(".profile-5-quote").css("display", "none");
+        $(".profile-5-color").css("top", "184px");
+    });
+
+    $(".profile-6").on("mouseenter", function() {
+        $(".profile-6-quote").css("display", "block");
+        $(".profile-6-color").css("top", "0");
+    });
+    $(".profile-6").on("mouseleave", function() {
+        $(".profile-6-quote").css("display", "none");
+        $(".profile-6-color").css("top", "240px");
+    });
+
+    $(".profile-7").on("mouseenter", function() {
+        $(".profile-7-quote").css("display", "block");
+        $(".profile-7-color").css("top", "0");
+    });
+    $(".profile-7").on("mouseleave", function() {
+        $(".profile-7-quote").css("display", "none");
+        $(".profile-7-color").css("top", "240px");
+    });
+
+    $(".profile-8").on("mouseenter", function() {
+        $(".profile-8-quote").css("display", "block");
+        $(".profile-8-color").css("top", "0");
+    });
+    $(".profile-8").on("mouseleave", function() {
+        $(".profile-8-quote").css("display", "none");
+        $(".profile-8-color").css("top", "272px");
+    });
+
+    $(".profile-9").on("mouseenter", function() {
+        $(".profile-9-quote").css("display", "block");
+        $(".profile-9-color").css("top", "0");
+    });
+    $(".profile-9").on("mouseleave", function() {
+        $(".profile-9-quote").css("display", "none");
+        $(".profile-9-color").css("top", "272px");
+    });
+});
